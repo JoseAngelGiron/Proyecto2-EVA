563fe7512b354696749973bbe130b75dca8d0b1e
Santiago García <santiagogarciaruiz665@gmail.com>
2024-02-29 14:03:02 +0100

    User Completo

--- Model/User.java
+++ Model/User.java
@@ -12,27 +12,11 @@ public class User {
 
     private final int MIN_LENGTH = 8;
     private final int MAX_LENGTH = 20;
-
-    public User(String name, String nickName, String email, String password) {
+    public User(String name, String nickName, String email, String password){
         setName(name);
         setNickName(nickName);
-        if (validateEmail(email)) {
-            setEmail(email);
-        } else {
-            this.email = "default@email.com";
-        }
+        setEmail(email);
         setPassword(password);
-    }
-
-    public User(String name, String nickName, String email) {
-        setName(name);
-        setNickName(nickName);
-        if (validateEmail(email)) {
-            setEmail(email);
-        } else {
-            this.email = "default@email.com";
-        }
-        this.password = "";
     }
 
     public String getName() {
@@ -54,16 +38,12 @@ public class User {
     public void setName(String name) {
         if (validateNoSpaces(name)) {
             this.name = name;
-        } else {
-            System.out.println("El nombre no puede contener espacios");
         }
     }
 
     public void setNickName(String nickName) {
         if (validateNoSpaces(nickName)) {
             this.nickName = nickName;
-        } else {
-            System.out.println("El apodo no puede contener espacios");
         }
     }
 
@@ -80,21 +60,27 @@ public class User {
         return isPasswordSet;
     }
 
+    /**
 
+     Este metodo valida si el name y el nickName no tiene espacios dentro de el campo.
+     @param text que se va a validar.
+     @return Si no cumple la funcion devolveria que el apodo no es valido*/
     private boolean validateNoSpaces(String text) {
         Pattern pattern = Pattern.compile("^\\S*$");
         return pattern.matcher(text).matches();
     }
+    /**
 
+     Este metodo valida si el email empieza con un texto y termina en .com o .es
+     @param email que se va a validar.
+     @return Si no cumple la funcion devolveria que el email no es valido*/
     private boolean validateEmail(String email) {
         Pattern pattern = Pattern.compile(".+@.+\\.(com|es)$");
+        if (pattern.matcher(email).matches()) {
+            this.email = email;
+        }
         return pattern.matcher(email).matches();
     }
-
-    private boolean passwordIntroducedIsStrong(String password) {
-        return true;
-    }
-
     /**
 
      Este metodo encripta la contraseña haciendo uso de la biblioteca hash-256
@@ -123,5 +109,62 @@ public class User {
             e.printStackTrace();
         }
         return hexString;
+    }
+    /**
+    Comprueba que la contraseña introducida, tiene al menos 3 digitos, 3 minúsculas y 1 mayúscula*
+    @param password la contraseña que se va a validar
+    @return true o false, en función de si la contraseña cumple con los requisitos o no*/
+    private boolean passwordIntroducedIsStrong(String password) {
+        boolean result;
+
+        result = (checkAmountLowerCase(password) && checkAmountUpperCase(password) && checkAmountOfNumbers(password));
+
+        return result;
+    }
+
+    /**
+
+    Función que comprueba si la contraseña que se le pasa tiene una cierta cantidad de números
+    @param password la contraseña a comprobar
+    @return true o false, en función si cumple con la condición*/
+    private boolean checkAmountOfNumbers(String password) {
+        int acu = 0;
+        for (int i = 0; i < password.length(); i++) {
+
+            if (password.charAt(i) >= 48 && password.charAt(i) <= 57) {
+                acu++;
+            }
+        }
+        return acu >= 3;
+    }
+    /**
+
+     Función que comprueba si la contraseña que se le pasa tiene una cierta cantidad de mayúsculas
+     @param password la contraseña a comprobar
+     @return true o false, en función si cumple con la condición*/
+    private boolean checkAmountUpperCase(String password) {
+        int acu = 0;
+        for (int i = 0; i < password.length(); i++) {
+
+            if (password.charAt(i) >= 65 && password.charAt(i) <= 90) {
+                acu++;
+            }
+        }
+        return acu >= 1;
+    }
+    /**
+     * Función que comprueba si la contraseña que se le pasa tiene una cierta cantidad de minúsculas
+     * @param password la contraseña a comprobar
+     * @return true o false, en función si cumple con la condición
+     */
+    private boolean checkAmountLowerCase(String password) {
+        int acu = 0;
+        for (int i = 0; i < password.length(); i++) {
+
+            if (password.charAt(i) >= 97 && password.charAt(i) <= 122) {
+                acu++;
+            }
+        }
+        return acu >= 3;
     }
 }
