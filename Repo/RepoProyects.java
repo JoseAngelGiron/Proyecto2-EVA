44d9d8f3579f3a075bfb1eac448fda45514a2bd8
Alberto <albertoromeropino2004@gmail.com>
2024-03-08 12:59:53 +0100

    Falta el update

--- Repo/RepoProyects.java
+++ Repo/RepoProyects.java
@@ -24,7 +24,7 @@ public class RepoProyects extends AbstractRepository<Proyect> implements IRepoPr
     /**
      * Obtiene un proyecto del repositorio mediante su identificador
      *
-     * @param id El identificador del proyecto a buscar
+     * @param id   El identificador del proyecto a buscar
      * @param user El usuario que se va a comprobar si es el creador
      * @return El proyecto encontrado, o null si no se encontró ningún proyecto con ese identificador
      */
@@ -61,12 +61,13 @@ public class RepoProyects extends AbstractRepository<Proyect> implements IRepoPr
 
     /**
      * Función encargada de actualizar los datos de un usuario a los proyectos que el ha creado.
+     *
      * @param newUser que se trata de los nuevos datos de usuario.
      */
     @Override
     public void update(User newUser) {
-        for(Proyect proyect:elements){
-            if(proyect.getProjectCreator().equals(newUser)){
+        for (Proyect proyect : elements) {
+            if (proyect.getProjectCreator().equals(newUser)) {
                 proyect.setProjectCreator(newUser);
             }
         }
@@ -76,13 +77,14 @@ public class RepoProyects extends AbstractRepository<Proyect> implements IRepoPr
     /**
      * Función encargada de actualizar las todas las tareas de los proyectos, en las cuales el usuario que recibe la
      * función es el colaborador
+     *
      * @param newUser los datos del nuevo usuario que se van a actualizar
      */
     private void updateTasks(User newUser) {
-        for(Proyect proyect:elements){
-            for (Task task: proyect.getElements()){
-                for(int i=0;i<task.getColaboratorToCharge().length;i++){
-                    if(task.getColaboratorToCharge()[i].equals(newUser)){
+        for (Proyect proyect : elements) {
+            for (Task task : proyect.getElements()) {
+                for (int i = 0; i < task.getColaboratorToCharge().length; i++) {
+                    if (task.getColaboratorToCharge()[i].equals(newUser)) {
                         task.getColaboratorToCharge()[i] = newUser;
                     }
                 }
@@ -114,6 +116,7 @@ public class RepoProyects extends AbstractRepository<Proyect> implements IRepoPr
     /**
      * Segun el usuario que se le pasa comprueba en que proyectos tiene al menos asignado una tarea
      * y devuelve aquellos proyectos en los que se encuentra
+     *
      * @param user El usuario que va a buscar los proyectos en los que esta
      * @return devuelve un arraylist de todos los proyectos en los que esta el usuario
      */
@@ -140,16 +143,45 @@ public class RepoProyects extends AbstractRepository<Proyect> implements IRepoPr
     //recorre todos los proyectos, comparando el id del proyecto, y si encaja, recorre el array de proyectos
     @Override
     public Proyect retrieveProyectIfColaborator(User user, String idProyecto) {
-        return null;
+        Proyect proyect = null;
+        for (Proyect proyects : elements) {
+            for (Task tmpuser : proyects.getElements()) {
+                for (int i = 0; i > tmpuser.getColaboratorToCharge().length; i++) {
+                    if (proyects.getId().equals(idProyecto) && tmpuser.getColaboratorToCharge()[i].equals(user)) {
+                        proyect = proyects;
+                    }
+                }
+            }
+        }
+        return proyect;
     }
 
     @Override
     public Task retrieveTask(User userToCharge, String idTask) {
+        Task task = null;
+        // que significa recuperar tarea
+        for (Proyect proyect : elements) {
+            for (Task tmptask : proyect.getElements()) {
+                for (int i = 0; i > tmptask.getColaboratorToCharge().length; i++) {
+                    if (tmptask.getId().equals(idTask)
+                            && tmptask.getColaboratorToCharge()[i].equals(userToCharge)) {
+                        task = tmptask;
+                    }
+                }
+            }
+        }
+
+
         return null;
     }
 
     @Override
     public boolean updateTask(Task task, Proyect proyect) {
-        return false;
+        boolean updated = false;
+        // que parametro es el que se actualiza de una tarea
+        // o es toda la tarea
+
+
+        return updated;
     }
 }
